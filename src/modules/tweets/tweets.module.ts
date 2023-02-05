285f77da286be074fb2992f9a150987b52a191b8
devjoaobatista <dev.joaobatista@gmail.com>
2023-02-05 10:09:17 -0300

    project end.

--- src/modules/tweets/tweets.module.ts
+++ src/modules/tweets/tweets.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/database/prisma.module';
 import { TweetsRepository } from './tweets.repository';
+import { TweetsService } from './tweets.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [TweetsRepository],
+  providers: [TweetsRepository, TweetsService],
+  exports: [TweetsService],
 })
 export class TweetsModule {}
-
